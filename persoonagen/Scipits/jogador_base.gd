6c44cb00645266e658fec1d69cbd4c3b947ead99
Pedroklemensov <80336621+PedroKlemensov@users.noreply.github.com>
2024-04-11 17:08:42 -0300

    Criaçao do personagem primcipal

---
+++ persoonagen/Scipits/jogador_base.gd
@@ -0,0 +1,18 @@
+extends CharacterBody2D
+
+@export_category("Variables")
+@export var _move_speed:float = 64.0
+
+
+func _physics_process(_delta: float) -> void:
+	_move()
+	
+
+func _move() -> void:
+	var _direction: Vector2 = Vector2(
+		Input.get_axis("move_leaft","move_right"),
+		Input.get_axis("move_up","move_down")
+	)
+	velocity = _direction.normalized() * _move_speed
+	move_and_slide()
+	prints(_direction)
